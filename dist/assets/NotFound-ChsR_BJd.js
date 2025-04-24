15432ed9d0e83898574f4c46af05216274cb4250
Nadhir Echabaan <nadhirchabane17@gmail.com>
2025-04-24 14:34:54 +0000

    removed dist

--- dist/assets/NotFound-ChsR_BJd.js
+++
@@ -1,1 +0,0 @@
-import{u as n,j as o}from"./index-ClQbt-RO.js";const a=()=>{const{t}=n("translation");return o.jsx("div",{className:"shared_not_found",children:t("not_found")})};export{a as default};
--- dist/assets/formatDate-DKgZRy0c.js
+++
@@ -1,1 +0,0 @@
-function o(a){const t=new Date(a),e=String(t.getUTCDate()).padStart(2,"0"),n=String(t.getUTCMonth()+1).padStart(2,"0"),r=t.getUTCFullYear();return`${e}/${n}/${r}`}export{o as f};
